b497ae2428f5eb08fec9ad15b5a6c7aaf5f72ca2
andytvang <andytvang@mail.fresnostate.edu>
2021-05-13 16:07:23 -0700

    finish pseudocode

--- final_1.py
+++ final_1.py
@@ -1,4 +1,4 @@
-"""
+"""Structured-English
 start
 
 create a program that displays the grades of the students on their final
@@ -7,12 +7,27 @@ above the average.
 
 Number of grades = 24.
 Average grade = 83.25
-Percentage of grades above average = 54.17%
+Percentage of grades above class average = 54.17%
 
-Use open method to bring the list in a dictionary
+Use open method to bring the list of grades in a dictionary
 Then, grades will be listed as integers
 Calculate grades
 
 end
 """
+"""Pseudo-Code
 
+main()
+
+    infile = open(txt)
+    display the number of grades
+    print("The amount of grades is")
+
+    display the average grade
+    print("Average grade is")
+
+    calculate
+    print("Percentage of grades above class average")
+    
+"""
+    
