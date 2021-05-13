5c2f7962a3a4cb0ba2e5683db183696df6fd0632
andytvang <andytvang@mail.fresnostate.edu>
2021-05-13 16:42:19 -0700

    finish exam-end

--- final_1.py
+++ final_1.py
@@ -11,13 +11,13 @@ Percentage of grades above class average = 54.17%
 
 Use open method to bring the list of grades in a dictionary
 Then, grades will be listed as integers
-Calculate grades
+We then calculate grades
 
 end
 """
 """Pseudo-Code
 
-main()
+def main():
 
     infile = open(txt)
     display the number of grades
@@ -26,8 +26,33 @@ main()
     display the average grade
     print("Average grade is")
 
-    calculate
+def calculate_percent_above_average():
     print("Percentage of grades above class average")
     
 """
-    
+def main():
+    infile = open("Final.txt" , 'r')
+    testGrades = [line.rstrip() for line in infile]
+    infile.close()
+    for i in range(len(testGrades)):
+        testGrades[i] = int(testGrades[i])
+    len(testGrades)
+    print("The amount of grades is: " , len(testGrades))
+
+    classgradeaverage = sum(testGrades) / len(testGrades)
+    print("Average grade is: " , classgradeaverage)
+    dict = testGrades
+    avg = classgradeaverage
+    calculate_percent_above_average(dict, avg)
+
+def calculate_percent_above_average(dict, avg):
+    num = 0
+    for grade in dict:
+        if grade > avg:
+            num += 1
+    print("Percentage of grades above class average: {0:.2f}%".format(100 * num / len(dict)))
+
+main()
+
+
+
