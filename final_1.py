f3c718dfbb8e51c2e1b8a26e4e8878dc32d9ae0b
andytvang <andytvang@mail.fresnostate.edu>
2021-05-13 15:55:22 -0700

    finish structured english

---
+++ final_1.py
@@ -0,0 +1,18 @@
+"""
+start
+
+create a program that displays the grades of the students on their final
+by listing the number of grades, the class average, and percentage of grades
+above the average.
+
+Number of grades = 24.
+Average grade = 83.25
+Percentage of grades above average = 54.17%
+
+Use open method to bring the list in a dictionary
+Then, grades will be listed as integers
+Calculate grades
+
+end
+"""
+
